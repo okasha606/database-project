f5853427eb2586c401b3c928be5ed5fb72eef0ce
mahmoud-badwy <mahmoudbadwysliem@gmail.com>
2026-05-02 14:38:12 +0300

    Refactor application to replace Form1 with Login form

--- projectdb/Login.Designer.cs
+++ projectdb/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace projectdb
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1012, 614);
+            Name = "Login";
+            Text = "Form1";
+            Load += this.Login_Load;
+            ResumeLayout(false);
         }
 
         #endregion
