f3594e9bae18a32395ab53e238a674b7c22c8c61
mahmoud-badwy <mahmoudbadwysliem@gmail.com>
2026-05-02 14:49:10 +0300

    Refactor SignUp form to include first name, last name, phone, address, latitude, and longitude fields

--- projectdb/SignUp.Designer.cs
+++ projectdb/SignUp.Designer.cs
@@ -17,31 +17,57 @@ namespace projectdb
 
         private void InitializeComponent()
         {
-            lblName = new Label();
-            txtName = new TextBox();
+            lblFirstName = new Label();
+            txtFirstName = new TextBox();
+            lblLastName = new Label();
+            txtLastName = new TextBox();
             lblEmail = new Label();
             txtEmail = new TextBox();
             lblPassword = new Label();
             txtPassword = new TextBox();
+            lblPhone = new Label();
+            txtPhone = new TextBox();
+            lblAddressName = new Label();
+            txtAddressName = new TextBox();
+            lblLat = new Label();
+            txtLat = new TextBox();
+            lblLong = new Label();
+            txtLong = new TextBox();
             btnSignUp = new Button();
             btnBackToLogin = new Button();
             SuspendLayout();
             // 
-            // lblName
+            // lblFirstName
             // 
-            lblName.AutoSize = true;
-            lblName.Location = new Point(350, 100);
-            lblName.Name = "lblName";
-            lblName.Size = new Size(63, 25);
-            lblName.TabIndex = 0;
-            lblName.Text = "Name:";
+            lblFirstName.AutoSize = true;
+            lblFirstName.Location = new Point(350, 50);
+            lblFirstName.Name = "lblFirstName";
+            lblFirstName.Size = new Size(101, 25);
+            lblFirstName.TabIndex = 0;
+            lblFirstName.Text = "First Name:";
             // 
-            // txtName
+            // txtFirstName
             // 
-            txtName.Location = new Point(450, 97);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(200, 31);
-            txtName.TabIndex = 1;
+            txtFirstName.Location = new Point(480, 47);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(200, 31);
+            txtFirstName.TabIndex = 1;
+            // 
+            // lblLastName
+            // 
+            lblLastName.AutoSize = true;
+            lblLastName.Location = new Point(350, 100);
+            lblLastName.Name = "lblLastName";
+            lblLastName.Size = new Size(99, 25);
+            lblLastName.TabIndex = 2;
+            lblLastName.Text = "Last Name:";
+            // 
+            // txtLastName
+            // 
+            txtLastName.Location = new Point(480, 97);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(200, 31);
+            txtLastName.TabIndex = 3;
             // 
             // lblEmail
             // 
@@ -49,15 +75,15 @@ namespace projectdb
             lblEmail.Location = new Point(350, 150);
             lblEmail.Name = "lblEmail";
             lblEmail.Size = new Size(58, 25);
-            lblEmail.TabIndex = 2;
+            lblEmail.TabIndex = 4;
             lblEmail.Text = "Email:";
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(450, 147);
+            txtEmail.Location = new Point(480, 147);
             txtEmail.Name = "txtEmail";
             txtEmail.Size = new Size(200, 31);
-            txtEmail.TabIndex = 3;
+            txtEmail.TabIndex = 5;
             // 
             // lblPassword
             // 
@@ -65,33 +91,97 @@ namespace projectdb
             lblPassword.Location = new Point(350, 200);
             lblPassword.Name = "lblPassword";
             lblPassword.Size = new Size(91, 25);
-            lblPassword.TabIndex = 4;
+            lblPassword.TabIndex = 6;
             lblPassword.Text = "Password:";
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(450, 197);
+            txtPassword.Location = new Point(480, 197);
             txtPassword.Name = "txtPassword";
             txtPassword.PasswordChar = '*';
             txtPassword.Size = new Size(200, 31);
-            txtPassword.TabIndex = 5;
+            txtPassword.TabIndex = 7;
+            // 
+            // lblPhone
+            // 
+            lblPhone.AutoSize = true;
+            lblPhone.Location = new Point(350, 250);
+            lblPhone.Name = "lblPhone";
+            lblPhone.Size = new Size(66, 25);
+            lblPhone.TabIndex = 8;
+            lblPhone.Text = "Phone:";
+            // 
+            // txtPhone
+            // 
+            txtPhone.Location = new Point(480, 247);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(200, 31);
+            txtPhone.TabIndex = 9;
+            // 
+            // lblAddressName
+            // 
+            lblAddressName.AutoSize = true;
+            lblAddressName.Location = new Point(350, 300);
+            lblAddressName.Name = "lblAddressName";
+            lblAddressName.Size = new Size(131, 25);
+            lblAddressName.TabIndex = 10;
+            lblAddressName.Text = "Address Name:";
+            // 
+            // txtAddressName
+            // 
+            txtAddressName.Location = new Point(480, 297);
+            txtAddressName.Name = "txtAddressName";
+            txtAddressName.Size = new Size(200, 31);
+            txtAddressName.TabIndex = 11;
+            // 
+            // lblLat
+            // 
+            lblLat.AutoSize = true;
+            lblLat.Location = new Point(350, 350);
+            lblLat.Name = "lblLat";
+            lblLat.Size = new Size(79, 25);
+            lblLat.TabIndex = 12;
+            lblLat.Text = "Latitude:";
+            // 
+            // txtLat
+            // 
+            txtLat.Location = new Point(480, 347);
+            txtLat.Name = "txtLat";
+            txtLat.Size = new Size(200, 31);
+            txtLat.TabIndex = 13;
+            // 
+            // lblLong
+            // 
+            lblLong.AutoSize = true;
+            lblLong.Location = new Point(350, 400);
+            lblLong.Name = "lblLong";
+            lblLong.Size = new Size(96, 25);
+            lblLong.TabIndex = 14;
+            lblLong.Text = "Longitude:";
+            // 
+            // txtLong
+            // 
+            txtLong.Location = new Point(480, 397);
+            txtLong.Name = "txtLong";
+            txtLong.Size = new Size(200, 31);
+            txtLong.TabIndex = 15;
             // 
             // btnSignUp
             // 
-            btnSignUp.Location = new Point(450, 250);
+            btnSignUp.Location = new Point(480, 460);
             btnSignUp.Name = "btnSignUp";
             btnSignUp.Size = new Size(90, 40);
-            btnSignUp.TabIndex = 6;
+            btnSignUp.TabIndex = 16;
             btnSignUp.Text = "Sign Up";
             btnSignUp.UseVisualStyleBackColor = true;
             btnSignUp.Click += btnSignUp_Click;
             // 
             // btnBackToLogin
             // 
-            btnBackToLogin.Location = new Point(550, 250);
+            btnBackToLogin.Location = new Point(580, 460);
             btnBackToLogin.Name = "btnBackToLogin";
             btnBackToLogin.Size = new Size(130, 40);
-            btnBackToLogin.TabIndex = 7;
+            btnBackToLogin.TabIndex = 17;
             btnBackToLogin.Text = "Back to Login";
             btnBackToLogin.UseVisualStyleBackColor = true;
             btnBackToLogin.Click += btnBackToLogin_Click;
@@ -103,12 +193,22 @@ namespace projectdb
             ClientSize = new Size(1012, 614);
             Controls.Add(btnBackToLogin);
             Controls.Add(btnSignUp);
+            Controls.Add(txtLong);
+            Controls.Add(lblLong);
+            Controls.Add(txtLat);
+            Controls.Add(lblLat);
+            Controls.Add(txtAddressName);
+            Controls.Add(lblAddressName);
+            Controls.Add(txtPhone);
+            Controls.Add(lblPhone);
             Controls.Add(txtPassword);
             Controls.Add(lblPassword);
             Controls.Add(txtEmail);
             Controls.Add(lblEmail);
-            Controls.Add(txtName);
-            Controls.Add(lblName);
+            Controls.Add(txtLastName);
+            Controls.Add(lblLastName);
+            Controls.Add(txtFirstName);
+            Controls.Add(lblFirstName);
             Name = "SignUp";
             Text = "Sign Up";
             ResumeLayout(false);
@@ -117,12 +217,22 @@ namespace projectdb
 
         #endregion
 
-        private Label lblName;
-        private TextBox txtName;
+        private Label lblFirstName;
+        private TextBox txtFirstName;
+        private Label lblLastName;
+        private TextBox txtLastName;
         private Label lblEmail;
         private TextBox txtEmail;
         private Label lblPassword;
         private TextBox txtPassword;
+        private Label lblPhone;
+        private TextBox txtPhone;
+        private Label lblAddressName;
+        private TextBox txtAddressName;
+        private Label lblLat;
+        private TextBox txtLat;
+        private Label lblLong;
+        private TextBox txtLong;
         private Button btnSignUp;
         private Button btnBackToLogin;
     }
