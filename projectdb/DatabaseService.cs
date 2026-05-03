46df199df59c0232fc1168034dea2d38566fdf8c
gergshany3 <24P0393@eng.asu.edu.eg>
2026-05-03 16:30:16 +0300

    modified connection string

--- projectdb/DatabaseService.cs
+++ projectdb/DatabaseService.cs
@@ -6,10 +6,18 @@ namespace projectdb
 {
     public class DatabaseService
     {
-        private readonly string _connectionString = @"Data Source=MAHMOUDBADWY\MSSQLSERVER01;Initial Catalog=project;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=""SQL Server Management Studio"";Command Timeout=0";
+        string badwy = @"Data Source=MAHMOUDBADWY\MSSQLSERVER01;Initial Catalog=project;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=""""SQL Server Management Studio"""";Command Timeout=0";
+        string gerges = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=project;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Application Name=""SQL Server Management Studio"";Command Timeout=2147483647";
+        
+        private string GetConnectionS()
+        {
+             
+             return gerges;
 
+        }
         public SqlConnection GetConnection()
         {
+            string _connectionString =  GetConnectionS();
             return new SqlConnection(_connectionString);
         }
     }
